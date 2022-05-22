25baf458fe111f56262c87021be9415f20784556
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-22 23:41:11 +0200

    Add inline getters

--- inc/Function.h
+++ inc/Function.h
@@ -18,6 +18,8 @@
 class Function : public Data
 {
 	protected:
+		std::vector<std::string> _comment_block;
+
 		bool _is_static;
 
 		bool _is_returned_value_const;
@@ -31,6 +33,16 @@ class Function : public Data
 		virtual ~Function() = default;
 
 		std::string to_Str(void) const override;
+
+		// ------------------------------------------------
+		// Inline Getters
+		// ------------------------------------------------
+		inline std::vector<std::string>			get_comment_block(void) const			{ return _comment_block; }
+		inline bool								get_is_static(void) const				{ return _is_static; }
+		inline bool								get_is_returned_value_const(void) const	{ return _is_returned_value_const; }
+		inline std::string						get_returned_value_type(void) const		{ return _returned_value_type; }
+		inline std::string						get_name(void) const					{ return _name; }
+		inline std::vector<Function_Parameter>	get_parameters(void) const				{ return _parameters; }
 };
 
 #endif // __FUNCTION_H__
