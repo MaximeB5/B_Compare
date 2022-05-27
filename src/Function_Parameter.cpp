2874d3c86f89bf4aa8a8a950b9465320ed7fac6a
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-27 17:50:21 +0200

    Modify _parameter_type management by adding it
    _parameter_type replaces the previous management of a vector of unique ptr of Data

--- src/Function_Parameter.cpp
+++ src/Function_Parameter.cpp
@@ -5,6 +5,7 @@
 #include "Function_Parameter.h"
 #include "Helper_Functions.h"
 
+#include <utility>
 #include <exception>
 
 // ------------------------------------------------
@@ -16,47 +17,12 @@
 /// </summary>
 /// <param name="const std::string & content"></param>
 Function_Parameter::Function_Parameter(const std::string & content)
-	: Data(content)
+	: Data(content), _parameter_type(content)
 {
-    std::vector<std::string> words;
+	std::size_t pos = content.find_last_of(" ");
 
-    // ------------------------------------------------
-    // Parse the string 'content' to get words
-    {
-        std::string s;
-
-        for (auto & c : content)
-        {
-            if (c == ' ') {
-                words.push_back(s);
-                s.clear();
-            }
-            else {
-                s += c;
-            }
-        }
-    }
-    // ------------------------------------------------
-    // Identify the words : are they type, qualifier or is it the name
-    {
-        for (auto const & str : words)
-        {
-            E_C_TYPE t      = Str_to_E_C_TYPE(str);
-            E_C_QUALIFIER q = Str_to_E_C_QUALIFIER(str);
-
-            if (t != E_C_TYPE::NONE)            { _content.push_back(std::make_unique<C_Type>(t)); }
-            else if (q != E_C_QUALIFIER::NONE)  { _content.push_back(std::make_unique<C_Qualifier>(q)); }
-            else if (_parameter_name.empty())   {
-                _parameter_name = str;
-                _content.push_back(std::make_unique<Data>(str));
-            }
-            else {
-                throw std::exception(
-                    std::string("Function_Parameter - Constructor : Words identification failed. Initial string is '" + content + "'").c_str()
-                );
-            }
-        }
-    }
+	_parameter_type.set_type(content.substr(0, pos));
+	_parameter_name = content.substr(pos + 1);
 }
 
 // ------------------------------------------------
@@ -64,7 +30,7 @@ Function_Parameter::Function_Parameter(const std::string & content)
 
 std::string Function_Parameter::to_Str(void) const
 {
-	return _data_content;
+	return std::string(_parameter_type.get_type() + " " + _parameter_name);
 }
 
 // ------------------------------------------------
