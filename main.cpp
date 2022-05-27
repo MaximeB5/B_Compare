554e5c7eb2c82406690dfdbcea12b2d1d5390334
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-27 17:38:24 +0200

    Add DEBUG define

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 
 #include "File_Reader.h"
 
-#include <exception>
+#include <stdexcept>
+
+// ------------------------------------------------
+// Debug & Tests
+// ------------------------------------------------
+
+#ifdef DEBUG
+	#include <iostream>
+#endif // DEBUG
 
 // ------------------------------------------------
 // Main
@@ -12,14 +20,15 @@
 
 int main()
 {
-	File_Reader reader_1{ "test.c" };
-	File_Reader reader_2{ "test.c" };
+	
+	File_Reader reader_1{"test.c"};
+	File_Reader reader_2{"test.c"};
 
 	if (!reader_1.is_ready_to_read()) { throw std::runtime_error("The file name or its path n°1 is incorrect."); }
 	if (!reader_2.is_ready_to_read()) { throw std::runtime_error("The file name or its path n°2 is incorrect."); }
 
 	reader_1.read_file();
 	reader_2.read_file();
-
+	
 	return EXIT_SUCCESS;
 }
