405e42353ebde6680463ef5440c9f67bda04ab75
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-27 17:51:00 +0200

    Create cpp file Parameter Type

---
+++ src/Parameter_Type.cpp
@@ -0,0 +1,18 @@
+// ------------------------------------------------
+// Includes
+// ------------------------------------------------
+
+#include "Parameter_Type.h"
+
+// ------------------------------------------------
+// Class Implementation
+// ------------------------------------------------
+
+/// <summary>
+/// Constructor
+/// </summary>
+/// <param name="content"></param>
+Parameter_Type::Parameter_Type(const std::string & content)
+	: Data(content), _content(content)
+{
+}
