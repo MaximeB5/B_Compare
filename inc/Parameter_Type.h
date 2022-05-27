46cf5fba81a02aecd87cab43e8f59edd0990b140
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-27 17:43:59 +0200

    Create header Parameter Type

---
+++ inc/Parameter_Type.h
@@ -0,0 +1,30 @@
+#ifndef __PARAMETER_TYPE_H__
+#define __PARAMETER_TYPE_H__
+
+// ------------------------------------------------
+// Includes
+// ------------------------------------------------
+
+#include "Data.h"
+
+#include <string>
+
+// ------------------------------------------------
+// Class Definition
+// ------------------------------------------------
+
+class Parameter_Type : public Data
+{
+	protected:
+		std::string _content;
+
+	public:
+		Parameter_Type() = delete;
+		Parameter_Type(const std::string & content);
+		virtual ~Parameter_Type() = default;
+
+		inline void set_type(std::string type) noexcept		{ _content = type; _data_content = type; };
+		inline std::string get_type(void) const noexcept	{ return _content; };
+};
+
+#endif // __PARAMETER_TYPE_H__
