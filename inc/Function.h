d07fe451b1ebe53eb428ea94a721750c2b7fc0ff
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-27 17:41:54 +0200

    Update constructor params + add _function_content

--- inc/Function.h
+++ inc/Function.h
@@ -20,6 +20,7 @@ class Function : public Data
 	protected:
 		std::vector<std::string> _comment_block;
 
+		/* Function's Signature - Begin */
 		bool _is_static;
 
 		bool _is_returned_value_const;
@@ -27,9 +28,13 @@ class Function : public Data
 
 		std::string _name;
 		std::vector<Function_Parameter> _parameters;
+		/* Function's Signature - End */
+
+		std::vector<std::string> _function_content;
 
 	public:
-		Function(const std::string & signature);
+		Function() = delete;
+		Function(const std::string & signature, const std::vector<std::string> & comment_block, const std::vector<std::string> & function_content);
 		virtual ~Function() = default;
 
 		std::string to_Str(void) const override;
@@ -37,12 +42,13 @@ class Function : public Data
 		// ------------------------------------------------
 		// Inline Getters
 		// ------------------------------------------------
-		inline std::vector<std::string>			get_comment_block(void) const			{ return _comment_block; }
-		inline bool								get_is_static(void) const				{ return _is_static; }
-		inline bool								get_is_returned_value_const(void) const	{ return _is_returned_value_const; }
-		inline std::string						get_returned_value_type(void) const		{ return _returned_value_type; }
-		inline std::string						get_name(void) const					{ return _name; }
-		inline std::vector<Function_Parameter>	get_parameters(void) const				{ return _parameters; }
+		inline std::vector<std::string>					get_comment_block(void) const			{ return _comment_block; }
+		inline bool										get_is_static(void) const				{ return _is_static; }
+		inline bool										get_is_returned_value_const(void) const	{ return _is_returned_value_const; }
+		inline std::string								get_returned_value_type(void) const		{ return _returned_value_type; }
+		inline std::string								get_name(void) const					{ return _name; }
+		inline std::vector<Function_Parameter>			get_parameters(void) const				{ return _parameters; }
+		inline std::vector<std::string>					get_function_content(void) const		{ return _function_content; }
 };
 
 #endif // __FUNCTION_H__
