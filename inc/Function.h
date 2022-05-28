e7440288befec6cee32f9027651ffb870ff2025a
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-28 18:00:02 +0200

    Add noexcept to inline getters

--- inc/Function.h
+++ inc/Function.h
@@ -42,13 +42,13 @@ class Function : public Data
 		// ------------------------------------------------
 		// Inline Getters
 		// ------------------------------------------------
-		inline std::vector<std::string>					get_comment_block(void) const			{ return _comment_block; }
-		inline bool										get_is_static(void) const				{ return _is_static; }
-		inline bool										get_is_returned_value_const(void) const	{ return _is_returned_value_const; }
-		inline std::string								get_returned_value_type(void) const		{ return _returned_value_type; }
-		inline std::string								get_name(void) const					{ return _name; }
-		inline std::vector<Function_Parameter>			get_parameters(void) const				{ return _parameters; }
-		inline std::vector<std::string>					get_function_content(void) const		{ return _function_content; }
+		inline std::vector<std::string>					get_comment_block(void)				const noexcept	{ return _comment_block; }
+		inline bool										get_is_static(void)					const noexcept	{ return _is_static; }
+		inline bool										get_is_returned_value_const(void)	const noexcept	{ return _is_returned_value_const; }
+		inline std::string								get_returned_value_type(void)		const noexcept	{ return _returned_value_type; }
+		inline std::string								get_name(void)						const noexcept	{ return _name; }
+		inline std::vector<Function_Parameter>			get_parameters(void)				const noexcept	{ return _parameters; }
+		inline std::vector<std::string>					get_function_content(void)			const noexcept	{ return _function_content; }
 };
 
 #endif // __FUNCTION_H__
