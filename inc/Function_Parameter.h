d13489bc9fe055d839d2ac13cc038fe5f028e135
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-29 10:59:12 +0200

    Add auto to inline getters

--- inc/Function_Parameter.h
+++ inc/Function_Parameter.h
@@ -24,13 +24,13 @@ class Function_Parameter : public Data
 		Function_Parameter(const std::string & content);
 		virtual ~Function_Parameter() = default;
 
-		std::string to_Str(void) const override;
+		inline std::string to_Str(void) const noexcept override { return _parameter_type.get_type() + " " + _parameter_name; }
 
 		// ------------------------------------------------
 		// Inline Getters
 		// ------------------------------------------------
-		inline std::string get_type(void) const noexcept { return _parameter_type.get_type(); }
-		inline std::string get_name(void) const noexcept { return _parameter_name; }
+		inline auto get_type(void) const noexcept { return _parameter_type.get_type(); }
+		inline auto get_name(void) const noexcept { return _parameter_name; }
 };
 
 #endif // __FUNCTION_PARAMETER_H__
