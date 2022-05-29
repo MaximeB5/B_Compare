9a9d6f5c5c3c208d93d48050d375fa4cbe565e08
Max <48251088+MaximeB5@users.noreply.github.com>
2022-05-29 10:59:53 +0200

    Add auto to get_type

--- inc/Parameter_Type.h
+++ inc/Parameter_Type.h
@@ -23,8 +23,10 @@ class Parameter_Type : public Data
 		Parameter_Type(const std::string & content);
 		virtual ~Parameter_Type() = default;
 
-		inline void set_type(std::string type) noexcept		{ _content = type; _data_content = type; };
-		inline std::string get_type(void) const noexcept	{ return _content; };
+		inline std::string to_Str(void) const noexcept override { return _content; }
+
+		inline void set_type(std::string type)	noexcept		{ _content = type; _data_content = type; };
+		inline auto get_type(void)				const noexcept	{ return _content; };
 };
 
 #endif // __PARAMETER_TYPE_H__
