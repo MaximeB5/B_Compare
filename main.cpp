74fc92ec710e89a4cabea0ddb41601b8737a832f
Max <48251088+MaximeB5@users.noreply.github.com>
2022-06-16 08:58:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 // ------------------------------------------------
 
 // Uncomment the following define if you need it for some tests
-//#define DEBUG
+#define DEBUG
 
 #ifdef DEBUG
 	#include "Function_Parameter.h"
@@ -27,13 +27,21 @@ int main()
 {
 
 	File_Reader reader_1{"test.c"};
-	File_Reader reader_2{"test.c"};
+	File_Reader reader_2{"test.h"};
 
-	if (!reader_1.is_ready_to_read()) { throw std::runtime_error("The file name or its path n°1 is incorrect."); }
-	if (!reader_2.is_ready_to_read()) { throw std::runtime_error("The file name or its path n°2 is incorrect."); }
+	// Do not check the stream status for debug and tests
+	#ifndef DEBUG
+		if (!reader_1.is_ready_to_read()) { throw std::runtime_error("The file name or its path n°1 is incorrect."); }
+		if (!reader_2.is_ready_to_read()) { throw std::runtime_error("The file name or its path n°2 is incorrect."); }
+	#endif // !DEBUG
 
 	reader_1.read_file();
 	reader_2.read_file();
+
+	// TODO
+	// Create Analyzer class that'll receive the File_Reader objects and re-order data based on what are the common stuff
+	// It should create vectors for all what's common, ordered on alphabetic names
+	// It should create vectors of what's unique to each file
 
 	return EXIT_SUCCESS;
 }
